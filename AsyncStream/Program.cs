9cb52e010fedd5a814c12d7a0d17819a7b3cf8d4
Antonio Maniero <maniero@klip.net>
2019-04-19 13:17:18 -0300

    AsyncStream adicionado

---
+++ AsyncStream/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncStream {
+    class Program {
+        static async Task Main() {
+            foreach (var number in await GetNumbers()) WriteLine(number);
+            await foreach (var number in GetNumbersAsync()) WriteLine(number);
+        }
+        static async Task<int[]> GetNumbers() {
+            var r = new int[10];
+            for (var i = 0; i < 10; i++) {
+                await Task.Delay(1000);
+                r[i] = i;
+            }
+            return r;
+        }
+        static async IAsyncEnumerable<int> GetNumbersAsync() {
+            for (var i = 0; i < 10; i++) {
+                await Task.Delay(1000);
+                yield return i;
+            }
+            static void Teste() {
+                IAsyncEnumerab1e<int> result = from item in enumerable
+                                               where item % 2 == 0
+                                               select async {
+                    await Task.Yield();
+                    return item * 2;
+                };
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Escrevendo C# Moderno #
+# Escrevendo C# Moderno
 
 Códigos usados na palestra. Só podem ser usados de forma adequada dentro do contexto da palestra. Como pode ser observado é uma base pra apresentação e é preciso uma sequência de ações para fazer cada parte funcionar, eles foram escritos para ir evoluindo, por isso os erros são propositais, eu vou consertando interativamente na demo. Ainda faltam algumas partes que preciso escrever.
